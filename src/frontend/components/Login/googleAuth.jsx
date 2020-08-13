581552d802693de0ec367a7337eb01d85c297a45
Mateusz <mateusz.fordas@gmail.com>
2020-08-13 19:04:00 +0200

    Login/logout buttons & user data
    1. Created login/logout buttons depends from auth status
    2. Get basic user profile information
    Login/logout buttons & user data
    
    1. Created login/logout buttons depends from auth status
    2. Get basic user profile information

--- src/frontend/components/Login/googleAuth.jsx
+++ src/frontend/components/Login/googleAuth.jsx
@@ -4,24 +4,48 @@ import '../../main_styling/main_styling.scss';
 
 const GoogleAuth = () => {
 
+    const [authObject, setAuthObject] = useState(null);
+    const [isLogged, setStatus] = useState(false);
+
     useEffect(() => {
-        
+
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
-                clientId: ,
+                clientId: '',
                 scope: 'email'
-            });
+            }).then(() => {
+                setAuthObject(window.gapi.auth2.getAuthInstance());
+            }
+            );
         });
-        
+
     }, []);
 
-    const makeAuth = () => {
-        const auth = window.gapi.auth2.getAuthInstance();
-        auth.signIn();
+    const makeAuth = async () => {
+        await authObject.signIn();
+        setStatus(authObject.isSignedIn.get());
+    }
+
+    const logOut = async () => {
+        await authObject.signOut();
+        setStatus(authObject.isSignedIn.get());
+    }
+
+    const checkData = () => {
+        console.log(authObject.currentUser.get().getAuthResponse().id_token);
+        console.log(authObject.currentUser.get().getBasicProfile());
+    }
+
+    const renderButton = () => {
+        return !isLogged ? <div className="button" onClick={() => makeAuth()}>LogIn with Google Account</div> :  <div className="button" onClick={() => logOut()}>LogOut</div>
     }
 
     return (
-        <div className="button" onClick={makeAuth}>works</div>
+        <>
+            {renderButton()}           
+            <div className="button" onClick={() => checkData()}>CheckData</div>
+            <div className="button">{isLogged ? `${authObject.currentUser.get().getBasicProfile().getName()}` : `Niezalogowany`}</div>
+        </>
     )
 };
 
